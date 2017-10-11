a78bf1738a91a26ab376e8175c72453f30490358
YokohamaNanryo_PC_Club <nanryoupc2017@yahoo.co.jp>
2017-10-11 16:19:00 +0900

    WPF
    hoge

--- ha/Program.cs
+++ ha/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
-namespace ConsoleApplication1
+
+public class fast
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	[STAThread]
+	static void Main()
+	{
+		var button = new Button {Content = "ここを押せ"};
+		button.Click +=(sender, e) => MessageBox.Show("ようこそ");
+		
+		var Win = new Window
+			
+		{
+			Title = "サンプルプログラム",
+			Width = 300,
+			Height = 200,
+			Content =  button
+		};
+		
+		var app = new Application();
+		app.Run(Win);
+	}
 }
